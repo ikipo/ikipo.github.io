6285e80a6689ba9021c6ccc81155a12c7b644561
ikipo <59876511+ikipo@users.noreply.github.com>
2020-02-11 00:43:46 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCDNIrdhAyxNomnhnlM-MMyoFVD3SeJfaU",
+    authDomain: "giaitoan-3e3ef.firebaseapp.com",
+    databaseURL: "https://giaitoan-3e3ef.firebaseio.com",
+    projectId: "giaitoan-3e3ef",
+    storageBucket: "giaitoan-3e3ef.appspot.com",
+    messagingSenderId: "906010712377",
+    appId: "1:906010712377:web:426c34ad586624eb62f0e8",
+    measurementId: "G-CZH6B46B20"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();

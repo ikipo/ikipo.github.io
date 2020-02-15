704f44e7b03247ef8961c7f21c8e8b4289c3bdf9
ikipo <59876511+ikipo@users.noreply.github.com>
2020-02-15 08:27:14 +0700

    Add files via upload

--- question.js
+++ question.js
@@ -4,25 +4,16 @@ if(fullname==""){
 window.location.href="login.html";
 }else{
 var ele = document.getElementById("what-new-content");
-var ele1 = document.getElementById("tag1-new-content");
-var ele2 = document.getElementById("tag2-new-content");
-var ele3 = document.getElementById("tag3-new-content");
-for(i=0;i<5;i++){
-showWhatNew(ele);
-showWhatNew(ele1);
-showWhatNew(ele2);
-showWhatNew(ele3);
-}
-}
-}
 
+showPost();
+}
+}
 function showAllWhatNew(){
 alert("");
 }
-function showWhatNew(ele){
+function showOnePost(ele,onePost){
 var href = "#";
-var title = "Câu hỏi";
-//var ele = document.getElementById("what-new-content");
+var title = onePost.title;
 var p = document.createElement("p");
 var a = document.createElement("a");
 a.className = "a";
@@ -30,4 +21,39 @@ a.href = href;
 a.innerHTML = title;
 p.appendChild(a);
 ele.appendChild(p);
+}
+function showPost(){
+var loop1 = 1;
+var loop2 = 1;
+var loop3 = 1;
+var ele1 = document.getElementById("tag1-new-content");
+var ele2 = document.getElementById("tag2-new-content");
+var ele3 = document.getElementById("tag3-new-content");
+var db = firebase.database();
+var ref = db.ref("Posts/");
+ref.on("value",function(snapshot){
+snapshot.forEach(function(childSnapshot){
+var onePost = childSnapshot.val();
+if(onePost.cap1==true||onePost.cap2==true||onePost.cap3==true||onePost.other==true){
+ if(onePost.cap1==true){
+  if(loop1<6){
+  showOnePost(ele1,onePost);
+  loop1++;
+  }
+ }if(onePost.cap2==true){
+  if(loop2<6){
+  showOnePost(ele2,onePost);
+  loop2++;
+  }
+ }if(onePost.cap3==true){
+  if(loop3<6){
+  showOnePost(ele3,onePost);
+  loop3++;
+  }
+ }
+}else{
+alert("other!"+onePost.cap1);
+}
+});
+});
 }
